7653dc0ffe435c3dbe12468d6f88278fc85cb24c
Ryosuke <ryosuke.san.hana@gmail.com>
2020-09-28 14:30:57 -0700

    docs(box): documented available props for component
    TS should display props with autocomplete, but added this comment for reference.

--- components/Box/Box.tsx
+++ components/Box/Box.tsx
@@ -1,6 +1,10 @@
 import { createBox } from "@shopify/restyle";
 import { Theme } from "../../themes/default";
 
+/**
+ * Available props:
+ * backgroundColor, opacity, visible, layout, spacing, border, shadow, position.
+ */
 const Box = createBox<Theme>();
 
 export default Box;
